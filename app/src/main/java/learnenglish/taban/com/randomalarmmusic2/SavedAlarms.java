7b9f4b74e25f345121ff665024d03513dcf28841
Tal <tal.bny3@gmail.com>
2018-12-12 23:21:49 +0200

    created the SavedAlarms activity

---
+++ app/src/main/java/learnenglish/taban/com/randomalarmmusic2/SavedAlarms.java
@@ -0,0 +1,13 @@
+package learnenglish.taban.com.randomalarmmusic2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedAlarms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_alarms);
+    }
+}
